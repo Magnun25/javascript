bc34f5ab507f2d5504133ba907011b3c6250ebd1
Rodrigo Barbosa Ponte <rodrigobponte@gmail.com>
2022-03-10 09:51:36 -0300

    Estudando JS

---
+++ Módulo D/ex001.js
@@ -0,0 +1,6 @@
+var vel = 30
+console.log (`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log (`Sua velocidade é maior do que a velocidade permitida. Você foi MULTADO!!`)
+}
+console.log (`Dirija sempre com o cinto de segurança!`)
---
+++ Módulo D/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log (`Você está vivendo no ${pais}`)
+if(pais == 'Brasil') {
+    console.log ('Você é Brasileiro')
+} else {
+    console.log ('Você é Estrangeiro')
+}
