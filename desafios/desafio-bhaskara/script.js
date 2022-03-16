d319fc38c0b2e193e6e80e348b4056b26dadd623
Rodrigo Barbosa Ponte <rodrigobponte@gmail.com>
2022-03-16 17:53:46 -0300

    Atualizei desafio bhaskara

--- desafios/desafio-bhaskara/script.js
+++ desafios/desafio-bhaskara/script.js
@@ -11,4 +11,14 @@ function calcular() {
     res.innerHTML += `<p>A equação atual é <strong>${a}x² + ${b}x + ${c} = 0</strong></p>`
     res.innerHTML += `<p>O cálculo realizado será <strong>∆ = ${b}² - 4 . ${a} . ${c}</strong> </p>`
     res.innerHTML += `<p>O valor calculado foi <mark><strong>∆ = ${delta}</strong></mark></p>`
+    if (delta > 0) {
+        var x1 = (-b + Math.sqrt(delta))/(2*a)
+        var x2 = (-b - Math.sqrt(delta))/(2*a)
+        res.innerHTML += `<p>A equação tem duas raízes Reais sendo <strong>x¹ = ${x1}</strong> e <strong>x² = ${x2}</strong></p>`
+    } else if (delta == 0){
+        var x = (-b)/2*a
+        res.innerHTML += `<p>A equação tem apenas uma raíz Real sendo <strong>x = ${x}</strong></p>`
+    } else {
+        res.innerHTML += `<p>A equação não possuí raízes reais, portanto <strong>x = ∅</strong></p>`
+    }
 }
